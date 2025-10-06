fc20ece6f94d176af2f61d1c08535de7467bc9dc
Martin Beyer <martin.beyer@physik.uni-giessen.de>
2025-10-06 22:06:21 +0200

    fix some type annotations

--- src/Team_indecisive/tests/test_data/testdata_base_vector_tricline.py
+++ src/Team_indecisive/tests/test_data/testdata_base_vector_tricline.py
@@ -1,9 +1,9 @@
 import numpy as np
 
-testdata: list[list, tuple] = []
+testdata: list[tuple[list[float], tuple[np.ndarray]]] = []
 
-input_0: list = [1.0, 1.0, 1.0, 90.0, 90.0, 90.0]
-expected_0: tuple = (
+input_0: list[float] = [1.0, 1.0, 1.0, 90.0, 90.0, 90.0]
+expected_0: tuple[np.ndarray] = (
     np.array([1.0, 0.0, 0.0]),
     np.array([6.123234e-17, 1.000000e00, 0.000000e00]),
     np.array([6.123234e-17, 6.123234e-17, 1.000000e00]),
--- src/Team_indecisive/tests/test_data/testdata_calculate_volume.py
+++ src/Team_indecisive/tests/test_data/testdata_calculate_volume.py
@@ -1,6 +1,6 @@
-testdata: list[list[float], tuple[float]] = []
+testdata: list[tuple[list[float], tuple[float]]] = []
 
 input_0: list[float] = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
-expected_0: tuple[float] = (0.0, 0.0, 0.0,0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
+expected_0: tuple[float] = (0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
 
 testdata.append((input_0, expected_0))
