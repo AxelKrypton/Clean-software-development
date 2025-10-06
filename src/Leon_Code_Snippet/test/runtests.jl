ea7b21f3bff6b81c50b252a562ad45e3d96d1589
leonsieke <leonsieke@t-online.de>
2025-10-06 21:35:19 +0200

    add more tests

--- src/Leon_Code_Snippet/test/runtests.jl
+++ src/Leon_Code_Snippet/test/runtests.jl
@@ -1,13 +1,30 @@
 using Powerweek
 using Test
 
-@testset "Running some tests.." begin
-	@test 1 == 1
-	@test 1 < 2
-end
+@testset "All tests" begin
+	@testset "Calling src/Powerweek.jl with mock data" begin
+		@test begin
+			test_success = true
+			try
+				run(`julia ../src/Powerweek.jl -i "../data" -o "./tmp"`)
+			catch
+				test_success = false
+			end
+			test_success
+		end
+		@test begin
+			test_success = !isempty(readdir("./tmp"))
+			if test_success
+				# Clean up mock output files produced during testing
+				rm("./tmp", recursive = true)
+			end
+			test_success
+		end
+	end
 
-@testset "Running some more tests.." begin
-	@test 1 != 2
+	@testset "Running some more tests..." begin
+		@test 1 != 2
+	end
 end
 
 
