c81c7915b2c7acd1d451f348f032d7ee92df903d
Martin Beyer <martin.beyer@physik.uni-giessen.de>
2025-10-06 22:31:09 +0200

    fix type annotations & format

--- src/Team_indecisive/tests/test_data/testdata_crystallite.py
+++ src/Team_indecisive/tests/test_data/testdata_crystallite.py
@@ -1,9 +1,13 @@
-import numpy as np
 import os
 
-testdata: list[tuple[tuple,float]] = []
+testdata: list[tuple[tuple[str, float, int, int], float]] = []
 
-input_0: tuple = [os.path.join(os.path.dirname(__file__), "../../DATA/SILIZIUM.DAT"),1.5406,30,10]
+input_0: tuple[str, float, int, int] = [
+    os.path.join(os.path.dirname(__file__), "../../DATA/SILIZIUM.DAT"),
+    1.5406,
+    30,
+    10,
+]
 expected_0: float = -3.6059854189874456e-34
 
-testdata.append((input_0,expected_0))
+testdata.append((input_0, expected_0))
--- src/Team_indecisive/tests/test_kristallite.py
+++ src/Team_indecisive/tests/test_kristallite.py
@@ -1,21 +1,23 @@
 import os
 import sys
-import numpy as np
 
+import numpy as np
 import pytest
-from math import isclose
 
 sys.path.insert(1, os.path.join(os.path.dirname(__file__), ".."))
 from Kristallite import crystallite
 from test_data import testdata_crystallite
 
-@pytest.mark.parametrize(("inp","expected"),testdata_crystallite.testdata)
-def test_crystallite(
-        inp:tuple,
-        expected:float
-        )-> None:
+
+@pytest.mark.parametrize(("inp", "expected"), testdata_crystallite.testdata)
+def test_crystallite(inp: tuple[str, float, int, int], expected: float) -> None:
     result: float = crystallite(*inp)
-    np.testing.assert_allclose(result, expected, rtol=1e-7,atol=0.0, 
-                               equal_nan=False,err_msg="Mismatch in tuple element",
-                               strict=True)
-    
+    np.testing.assert_allclose(
+        result,
+        expected,
+        rtol=1e-7,
+        atol=0.0,
+        equal_nan=False,
+        err_msg="Crystallite value not matchings",
+        strict=True,
+    )
