a5ab526fa587201ffcc9330ed4f7c401dfcdf895
Jannik Petersen <japeters@uni-mainz.de>
2025-10-07 21:02:41 +0200

    Added missing include to run with gcc 13+

--- src/AKKJ-Build-Breaker/external/vir/test.h
+++ src/AKKJ-Build-Breaker/external/vir/test.h
@@ -45,6 +45,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <sstream>
 #include <typeinfo>
 #include <vector>
+#include <cstdint> 
 
 #ifdef HAVE_CXX_ABI_H
 #include <cxxabi.h>
