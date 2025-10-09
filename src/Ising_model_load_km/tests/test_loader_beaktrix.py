28fd607e4aea670c990a5b033225a269ccfc6a14
KScharmann <karina.scharmann@gmx.de>
2025-10-09 17:10:19 +0200

    apply formater & small corrections, include function description for loader_beaktrix

--- src/Ising_model_load_km/tests/test_loader_beaktrix.py
+++ src/Ising_model_load_km/tests/test_loader_beaktrix.py
@@ -9,40 +9,53 @@ from src.loader_beaktrix import loader_beaktrix
 
 data_type = tuple[int, list[float], list[np.ndarray], list[np.ndarray]]
 
+
 @pytest.fixture(scope="module")
 def get_data_loader_beaktrix() -> data_type:
     return loader_beaktrix()
 
-def test_L(get_data_loader_beaktrix: data_type) -> None:
-    L, _, _, _ = get_data_loader_beaktrix
-    assert L == 16
-    
+
+def test_l(get_data_loader_beaktrix: data_type) -> None:
+    output_l, _, _, _ = get_data_loader_beaktrix
+    expected_l = 16
+    assert output_l == expected_l
+
+
 def test_length_temperatures(get_data_loader_beaktrix: data_type) -> None:
     _, temperatures, _, _ = get_data_loader_beaktrix
-    assert len(temperatures) == 28
-    
+    expected_number_of_files = 28
+    assert len(temperatures) == expected_number_of_files
+
+
 def test_length_energy_densities(get_data_loader_beaktrix: data_type) -> None:
     _, _, energy_densities, _ = get_data_loader_beaktrix
-    assert len(energy_densities) == 28
+    expected_number_of_files = 28
+    assert len(energy_densities) == expected_number_of_files
+
 
 def test_length_magnetizations(get_data_loader_beaktrix: data_type) -> None:
     _, _, _, magnetizations = get_data_loader_beaktrix
-    assert len(magnetizations) == 28
-    
-def test_length_fileT2p25(get_data_loader_beaktrix: data_type) -> None:
-    _, temperatures, energy_densities, magnetizations = get_data_loader_beaktrix
-    file_index_T2p25 = 8
-    assert len(energy_densities[file_index_T2p25]) == 3865
-    assert len(magnetizations[file_index_T2p25]) == 3865
+    expected_number_of_files = 28
+    assert len(magnetizations) == expected_number_of_files
 
-def test_last_line_fileT2p25(get_data_loader_beaktrix: data_type) -> None:
-    _, temperatures, energy_densities, magnetizations = get_data_loader_beaktrix
-    file_index_T2p25 = 8
-    assert abs(energy_densities[file_index_T2p25][-1] - -2.84375) < 1e-5
-    assert abs(magnetizations[file_index_T2p25][-1] - 0.75) < 1e-5
 
-def test_first_line_fileT2p25(get_data_loader_beaktrix: data_type) -> None:
-    _, temperatures, energy_densities, magnetizations = get_data_loader_beaktrix
-    file_index_T2p25 = 8
-    assert abs(energy_densities[file_index_T2p25][0] - -3.03125) < 1e-5
-    assert abs(magnetizations[file_index_T2p25][0] - 0.6796875) < 1e-5
+def test_length_file_t2p25(get_data_loader_beaktrix: data_type) -> None:
+    _, _, energy_densities, magnetizations = get_data_loader_beaktrix
+    file_index_t2p25 = 8
+    expected_number_of_lines = 3865
+    assert len(energy_densities[file_index_t2p25]) == expected_number_of_lines
+    assert len(magnetizations[file_index_t2p25]) == expected_number_of_lines
+
+
+def test_last_line_file_t2p25(get_data_loader_beaktrix: data_type) -> None:
+    _, _, energy_densities, magnetizations = get_data_loader_beaktrix
+    file_index_t2p25 = 8
+    np.testing.assert_almost_equal(energy_densities[file_index_t2p25][-1], -2.84375)
+    np.testing.assert_almost_equal(magnetizations[file_index_t2p25][-1], 0.75)
+
+
+def test_first_line_file_t2p25(get_data_loader_beaktrix: data_type) -> None:
+    _, _, energy_densities, magnetizations = get_data_loader_beaktrix
+    file_index_t2p25 = 8
+    np.testing.assert_almost_equal(energy_densities[file_index_t2p25][0], -3.03125)
+    np.testing.assert_almost_equal(magnetizations[file_index_t2p25][0], 0.6796875)
