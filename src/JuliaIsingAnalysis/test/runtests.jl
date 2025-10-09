36a7b400ae0d6d1e0d2cec1d35f6f911349601e4
lesmah <hlessamacedo@gmail.com>
2025-10-09 13:49:13 +0200

    Minor refactoring and CheckDensities test

--- src/JuliaIsingAnalysis/test/runtests.jl
+++ src/JuliaIsingAnalysis/test/runtests.jl
@@ -1,2 +1,28 @@
-using JuliaIsingAnalysis
 using Test
+
+include("../src/JuliaIsingAnalysis.jl")
+
+function checkDensities(clmn,min,max)
+    flag = false
+    for element in clmn 
+        if element < min || element > max
+            flag = true 
+            break
+        end 
+    end 
+    return flag
+end
+
+
+
+
+@testset "All tests" begin 
+
+    @testset "Energy density should be between -1 and 1" begin
+        @test !checkDensities(Eggwin_df.e,-1,1)
+    end
+
+    
+
+
+end
