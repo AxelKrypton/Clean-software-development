92bc47009c378dd5f7f681178e9992452fda344d
Aniko Tim Falk <gd1875@uni-giessen.de>
2025-10-09 12:36:13 +0200

    Fixes on parse test

--- src/Philiko/tests/test_format_and_average.py
+++ src/Philiko/tests/test_format_and_average.py
@@ -1,7 +1,11 @@
 import pytest
 import pandas as pd
+import os
+import pathlib as Path
 
-from src.format_and_average 
+from src.format_and_average import input_parser
+
+test_data_dir = os.path.join(Path.Path(__file__).parent.parent,"test_data")
 
 #test parsed format
 #no duplicate configurations
@@ -11,7 +15,8 @@ from src.format_and_average
 #(check Energy density is roughly 4 times magnetization density for first ~100 elements at lowest temperature)
 
 def test_parsed():
-	df_parsed = input_parser("../test_data/simple_example.txt")
+	print(test_data_dir)
+	df_parsed = input_parser(os.path.join(test_data_dir,"simple_example.txt"))
 	df_expected = pd.DataFrame({"m": [0.1,0.4], "E": [0.1,0.4]})
 	df_expected
 	assert df_parsed == df_expected
