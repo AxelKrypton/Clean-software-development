030392d94bb45a54a41b0b351b1a530354f9ecb0
leonsieke <leonsieke@t-online.de>
2025-10-09 11:41:07 +0200

    add stuff

--- src/JuliaIsingAnalysis/src/JuliaIsingAnalysis.jl
+++ src/JuliaIsingAnalysis/src/JuliaIsingAnalysis.jl
@@ -1,5 +1,24 @@
 module JuliaIsingAnalysis
 
-greet() = print("Hello World!")
+using DelimitedFiles, DataFrames
+
+path = "../../Ising2D/Eggwin/Ising_2D_MCMC_Ns8_Ns8/temp0.5.txt"
+
+
+function readEggwinFile(path::String)
+
+	data, header = readdlm(path, header = true);
+	df = DataFrame(data[:, 1:3], ["Configuration", "E", "M"])
+
+	V = 8*8
+	T = split(split(path, "temp")[2], ".txt")[1]
+
+	df["T"] = T
+	df["V"] = V
+end
+
+function readBeaktrixFile(path::String)
+end
+
 
 end # module JuliaIsingAnalysis
---
+++ src/JuliaIsingAnalysis/test/runtests.jl
@@ -0,0 +1,2 @@
+using JuliaIsingAnalysis
+using Test
